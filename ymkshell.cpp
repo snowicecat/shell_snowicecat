ec3ccb0954d4ab01e0850574702050e1e0f313a0
snowicecat <snowicecat@126.com>
2015-10-21 15:38:09 +0800

    divide commend line

--- ymkshell.cpp
+++ ymkshell.cpp
@@ -11,9 +11,67 @@
 #include <signal.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <vector>
+#include <stdio.h>
 
 using namespace std;
+struct sigaction act;
+string commendLine;
+vector<string> commendSet;
+int numberOfCommendSet;
+// int pid = -2;
+
+void handleCommendLine(string commendLine);
+// void sighandler(int signum){
+// 	pid = -2;
+// 	cout << endl;
+// }
 
 int main() {
-	
+	// act.sa_handler = sighandler;
+	// sigemptyset(&act.sa_mask);
+	// act.sa_flags = 0;
+	// sigaction(SIGINT, &act, NULL);
+	// pid = -2;
+
+	while (1) {
+		numberOfCommendSet = 0;
+		cout << "Yanminkuan: " << get_current_dir_name() << "% ";
+		getline(cin, commendLine);
+		handleCommendLine(commendLine);
+		commendSet.clear();
+	}
+
+	return 0;
+}
+
+void handleCommendLine(string commendLine) {
+
+	if(commendLine == "exit" || commendLine == "q")
+		exit(0);
+	if(commendLine == "")
+		return;
+
+	//divide commendLine if pipeline
+	int lengthOfString = commendLine.size();
+	for (int i=0; i < lengthOfString; i++) {
+		int positionOfPipe = commendLine.find('|', 0);
+		if(positionOfPipe == -1) {
+			commendSet.push_back(commendLine);
+			break;
+		} else {
+			numberOfCommendSet++;
+			//push back the previous commendLine before |
+			commendSet.push_back(commendLine.substr(0, positionOfPipe));
+			//commendLine = line after |
+			commendLine = commendLine.substr(positionOfPipe+1, commendLine.size()-positionOfPipe+1);
+		}
+	}
+
+	if(numberOfCommendSet == 0) {
+		
+	}
+
+	// pid = -2;
+
 }
